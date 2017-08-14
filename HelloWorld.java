f67ffe65b440104d6f53d238d1fe55283dbaabe5
sudheer7700 <sudheergundra@gmail.com>
2017-08-14 08:39:49 -0400

    Added morning Apple to print

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ class HelloWorld
 		System.out.println("Hello World");
 		System.out.println("Goodmorning Sudheer");
 		System.out.println("Goodmorning Miracle");
+		System.out.println("Goodmorning Apple");
 	}
 }
