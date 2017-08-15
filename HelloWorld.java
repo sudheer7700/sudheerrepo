9e4d78e2381548961fb486f4c006415a1bffc5ee
sudheer7700 <sudheergundra@gmail.com>
2017-08-15 08:58:51 -0400

    Added time 8:58 AM

--- HelloWorld.java
+++ HelloWorld.java
@@ -12,5 +12,6 @@ class HelloWorld
 		System.out.println("Time is 8:15 AM EST");
 		System.out.println("Time is 8:47 AM EST");
 		System.out.println("Time is 8:51 AM EST");
+		System.out.println("Time is 8:58 AM EST");
 	}
 }
