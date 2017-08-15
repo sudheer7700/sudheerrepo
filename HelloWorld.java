13a9411085e06f681569a80f42d0cb2f57ff08c5
sudheer7700 <sudheergundra@gmail.com>
2017-08-15 07:56:05 -0400

    Added time

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ class HelloWorld
 		System.out.println("Hello World");
 		System.out.println("Goodmorning Sudheer");
 		System.out.println("Today is Aug 15");
+		System.out.println("Time is 7:55 AM EST");
 	}
 }
